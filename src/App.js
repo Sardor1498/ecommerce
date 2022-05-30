856f6ab5e072a6e96506afeb74f8543a1e471607
Sardor1498 <tsardor1498@gmail.com>
2022-05-30 18:38:24 +0500

    Sardor1498

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
+const Title = styled.h1`
+  color: red;
+  font-size: 52px;
+  font-weight: 900;
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Title>
+     SALOM
+    </Title>
   );
 }
 
